a7f7f5eb52d0570b8e02e3600980e49a8796d259
Eduardo-Valesi <duduvalesi@gmail.com>
2024-11-26 13:20:17 -0300

    updating

---
+++ ex01/aula09/index.js
@@ -0,0 +1,10 @@
+
+var corpo = document.body
+var p1 = document.getElementsByTagName('p')[1]
+/* var d = document.getElementById('msg')
+
+d.style.background = 'green'
+d.innerText = 'Estou aguardando...'
+document.getElementById('msg').innerText = 'Olá' */
+
+var d = document.querySelector('div#msg')
